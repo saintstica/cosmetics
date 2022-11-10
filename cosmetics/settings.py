e75f48f99115c9f667c6b980af440e03c2125d3c
unknown <saintstica@gmail.com>
2022-11-10 13:52:16 +0100

    postgresql installed

--- cosmetics/settings.py
+++ cosmetics/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'cosmetics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cosmeticsdb',
+        'USER': 'cosmeticsadmin',
+        'PASSWORD': 'admin1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
